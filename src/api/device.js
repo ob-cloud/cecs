a0d24140e01a21370fccd671ec82cd6eebe8a176
eamiear <justcall_me_k@163.com>
2019-08-07 15:17:10 +0800

    设备接口查询传参

--- src/api/device.js
+++ src/api/device.js
@@ -2,17 +2,22 @@
  * @Author: eamiear
  * @Date: 2019-08-05 17:32:41
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-08-06 15:07:33
+ * @Last Modified time: 2019-08-07 15:14:51
  */
 
 import {request} from '@/common/request'
 
 const DeviceAPI = {
-  getDeviceList (page = 0, pageSize = 10) {
+  getDeviceList (params) {
+    const page = params.pageNo
+    const pageSize = params.pageSize
+    delete params.pageNo
+    delete params.pageSize
     return request.get('/consumer/common', {
       CMD: 'query_device',
-      start_index: 0,
-      count: 10
+      start_index: page,
+      count: pageSize,
+      ...params
     })
   }
 }
