0d738ed37ba91487e8ce8a2e84d48fcc94ad833b
eamiear <justcall_me_k@163.com>
2019-08-23 11:32:20 +0800

    添加门锁接口、WiFi设备接口

--- src/api/device.js
+++ src/api/device.js
@@ -2,7 +2,7 @@
  * @Author: eamiear
  * @Date: 2019-08-05 17:32:41
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-08-16 16:15:01
+ * @Last Modified time: 2019-08-23 11:14:36
  */
 
 import {request} from '@/common/request'
@@ -53,4 +53,70 @@ const DeviceAPI = {
     })
   }
 }
-export default DeviceAPI
+const doorLock = {
+  getDoorLockList (serialId) {
+    return request.postForm('/consumer/common', {
+      CMD: 'query_intelligent_fingerHome',
+      serialId
+    })
+  },
+  getDoorLockOpenRecord (serialId) {
+    return request.postForm('/consumer/common', {
+      CMD: 'query_intelligent_openRecord',
+      serialId
+    })
+  },
+  getDoorLockAlarmRecord (serialId) {
+    return request.postForm('/consumer/common', {
+      CMD: 'query_intelligent_warningRecord',
+      serialId
+    })
+  },
+  getDoorLockUserList (serialId) {
+    return request.postForm('/consumer/common', {
+      CMD: 'query_intelligent_useringRecord',
+      serialId
+    })
+  },
+  /**
+  * @param {*} params
+  * params.serialId
+  * params.pin
+  * params.nickName
+  * params.mobile
+  * params.validateCode
+  */
+  updateDoorLockUserInfo (params) {
+    return request.postForm('/consumer/common', {
+      CMD: 'edit_intelligent_user',
+      ...params
+    })
+  },
+  /**
+   *
+   * @param {*} params
+   * params.pin
+   * params.mobile
+   */
+  getDoorLockCaptcha (params) {
+    return request.postForm('/consumer/common', {
+      CMD: 'end_intelligent_validateCode',
+      ...params
+    })
+  }
+}
+
+const LampAPI = {
+  openLamp () {
+
+  }
+}
+
+const WifiDeviceAPI = {
+  getWifiDeviceList () {
+    return request.postForm('/consumer/common', {
+      CMD: 'query_ali_dev'
+    })
+  }
+}
+export default {...DeviceAPI, ...doorLock, ...LampAPI, ...WifiDeviceAPI}
