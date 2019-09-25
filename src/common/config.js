81eb2be16fcb9e3f429e8b82f847119afd11cc26
eamiear <justcall_me_k@163.com>
2019-09-25 17:39:11 +0800

    部署到iot

--- src/common/config.js
+++ src/common/config.js
@@ -2,7 +2,7 @@
  * @Author: eamiear
  * @Date: 2019-02-19 10:38:23
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-09-17 14:33:59
+ * @Last Modified time: 2019-09-25 17:38:59
  */
 
 const config = {
@@ -10,7 +10,8 @@ const config = {
     baseApi: 'http://localhost:8081/'
   },
   prod: {
-    baseApi: 'https://192.168.200.254'
+    // baseApi: 'https://192.168.200.254'
+    baseApi: 'https://aliiot.on-bright.com'
   }
 }
 export default config
