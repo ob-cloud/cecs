9525da6554b898295798f938ca5608ecce67c65c
eamiear <justcall_me_k@163.com>
2019-10-22 16:49:05 +0800

    地图

--- src/views/room/map.vue
+++ src/views/room/map.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
+  <div class="map-wrapper">
+    <div class="map-toolbar">
+      <el-upload
+        class="upload-btn"
+        action="https://jsonplaceholder.typicode.com/posts/">
+        <el-button size="small" type="primary" icon="el-icon-upload">点击上传</el-button>
+        <!-- <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，图片规格 1080x700 </div> -->
+      </el-upload>
+      <el-button size="small" type="primary" icon="el-icon-edit">编辑</el-button>
+    </div>
+    <div class="map-content">
+      <div class="image-wrapper">
 
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +24,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.map-wrapper{
+  padding-left: 64px;
+  .map-toolbar{
+    text-align: right;
+    padding: 10px;
 
+    .upload-btn{
+      display: inline-block;
+    }
+    .el-button{
+      margin-left: 10px;
+    }
+  }
+  .map-content{
+    overflow: auto;
+
+    .image-wrapper{
+      width: 1080px;
+      height: 900px;
+      max-width: 1080px;
+      max-height: 700px;
+      border: 1px solid #999;
+    }
+  }
+}
 </style>
