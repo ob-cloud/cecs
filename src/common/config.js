f37a188d5fa87f0d96fdada20d098f9635977992
eamiear <justcall_me_k@163.com>
2020-01-02 11:30:26 +0800

    baseApi 自动使用当前服务IP

--- src/common/config.js
+++ src/common/config.js
@@ -2,7 +2,7 @@
  * @Author: eamiear
  * @Date: 2019-02-19 10:38:23
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-12-16 10:19:56
+ * @Last Modified time: 2020-01-02 10:59:35
  */
 
 const config = {
@@ -10,8 +10,9 @@ const config = {
     baseApi: 'http://localhost:8080/'
   },
   prod: {
-    baseApi: 'https://203.67.23.180'
+    // baseApi: 'https://203.67.23.180'
     // baseApi: 'https://192.168.200.254'
+    baseApi: ''
   }
 }
 export default config
